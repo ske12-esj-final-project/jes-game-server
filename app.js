9a21216a4b072345fe55118d5fb7384fdd336394
Nattapat <nattapat@Nattapats-MacBook-Pro.local>
2018-02-07 17:47:42 +0700

    change default port

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ io.on('connection', (socket) => {
     })
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 server.listen(PORT, () => {
     console.log(`Lisnten on http://localhost:${PORT}`)
     gameInterval = gameManager.createGameInterval()

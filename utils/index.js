2f0dfc9ab861d0dcd7d5bf169d1584bf0fdd2ada
b5710546232 <nattapat.su@ku.th>
2018-04-01 00:51:48 +0700

    add item random type with weight

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,11 @@
+const _ = require("lodash")
 const getRandomInt = (max) => {
     return Math.floor(Math.random() * Math.floor(max));
 }
+const sumList = (list) => {
+    return _.map(list).reduce((a, b) => a + b, 0);
+  }
 module.exports = {
-    getRandomInt
+    getRandomInt,
+    sumList
 }

a36f768a6e4fe09fb68869369274997e086f27b8
b5710546232 <nattapat.su@ku.th>
2018-04-08 16:19:15 +0700

    add logs

--- managers/room.js
+++ managers/room.js
@@ -28,12 +28,13 @@ module.exports = class {
         else {
             // check token is exist
             let players = GameManager.getPlayers()
-            for (let p in players) {
+            for (let key in players) {
+                let p = players[key]
                 console.log('p.token', p.userID, userID, userID === p.userID,p)
-                // if (userID === p.userID) {
-                //     console.log('check-token-should-be-error', p.userID)
-                //     return new Error("userID is exited")
-                // }
+                if (userID === p.userID) {
+                    console.log('check-token-should-be-error', p.userID)
+                    return new Error("userID is exited")
+                }
             }
 
         }

1251f4c2e2879f8fd3fb6727dda3b7f49d586c96
Raksit Mantanacharu <raksit.m@ku.th>
2018-04-11 14:56:14 +0700

    fixed conflicts

--- utils/createEquitmentItemList.js
+++ utils/createEquitmentItemList.js
@@ -1,22 +1,77 @@
-/**
- * my sandbox
- */
 const _ = require("lodash")
+const shortid = require('shortid')
 
-const equitmentData = require("../data/equipments")
-const { createWeaponItemList,assignRandomPositions ,createBulletList} = require('../utils/createEquitmentItemList')
-const SPAWNPOINTS = require('../spawnpoints/spawnpoint.json')
+const { sumList, getRandomInt } = require("./")
 
-// let itemSize = 91
-// let mockWeightPercentList = createWeaponItemList(itemSize, equitmentData)
+const createItemList = (numberOfItem, itemData) => {
+    const weightList = _.map(itemData, m => m.weight)
+    let targetList = _.map(itemData, w => {
+        let total = sumList(weightList)
+        return Object.assign(w, {
+            percentWeight: w.weight / total * numberOfItem
+        })
+    }).reduce((a, b) => {
+        let item = _.clone(b)
+        newArray = new Array(Math.ceil(b.percentWeight))
+        newArray = _.map(newArray, i => _.clone(item))
+        return _.concat(a, newArray)
+    }, [])
+    let diff = _.size(targetList) - numberOfItem
+    if (diff == 0)
+        return targetList
+    // delete item  if the list is exceed numberOfitem 
+    for (let i = 0; i < diff; i++) {
+        let randIndex = getRandomInt(numberOfItem)
+        targetList.splice(randIndex, 1)
+    }
+    return targetList
+}
 
-// console.log(mockWeightPercentList)
-// console.log(_.size(mockWeightPercentList), itemSize)
+const createWeaponItemList = (numberOfItem, itemData) => {
+    let equitmentDataExceptHand = _.filter(itemData, e => e.Index !== 0)
+    let itemWeightPercentList = createItemList(numberOfItem, equitmentDataExceptHand)
+    return _.map(itemWeightPercentList, m =>
+        ({ uid: shortid.generate(), weaponIndex: m.Index, position: {} }))
+}
 
-let itemSize = 100
-this.itemList = createWeaponItemList(itemSize, equitmentData)
+const assignRandomPositions = (items, spawnPoints) =>{
+    items = items.map(i => _.clone(i))
+    let t_points = spawnPoints.map(i => _.clone(i))
 
-this.equipments = assignRandomPositions(this.itemList, SPAWNPOINTS)
+    return items.map((item) => {
+        let index = getRandomInt(t_points.length)
+        item.position = _.clone(t_points[index])
+        item.uid = shortid.generate()
+        let o = t_points[index]
+        t_points = _.filter(t_points, target => target !== o)
+        return item
+    })
+}
+const createBulletList = (equitments)=>{
+    return _.map(equitments,e=>{
+        let {x,y,z} = e.position
+        // console.log(x,y)
+        const getRandomVal = (n)=>{
+            const fixed2Dec = (n)=> Math.round(n * 100)/100;
+            const range = getRandomInt(3)+1 *.1
+            const rand_sign = getRandomInt(2)?1:-1
+            return  fixed2Dec(n + (rand_sign*range))
+        }
+        // y = getRandomVal(y)
+        x = getRandomVal(x)
+        z = getRandomVal(z)
+        
+        return {
+            uid:shortid.generate(),
+            x:x,
+            y:y,
+            z:z
+        }
+    })
+}
 
-this.bulletList = createBulletList(this.equipments)
-console.log(this.bulletList)
+module.exports = {
+    createWeaponItemList,
+    assignRandomPositions,
+    createBulletList
+}

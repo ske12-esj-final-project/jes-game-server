d4dddc04ea6604c31c109d95a5d80f6672896033
b5710546232 <nattapat.su@ku.th>
2018-04-13 18:53:02 +0700

    parse

--- managers/player.js
+++ managers/player.js
@@ -44,7 +44,9 @@ module.exports = class {
         this.numberOfKill = 0
     }
     setupPlayer(data) {
+        data = JSON.parse(data)
         console.log('data setup-player',data)
+        this.cloathIndex = data[0]
         this.reset()
         this.position = { x: 0, y: 20, z: 0 }
         this.sendPlayersDataCreateCharacter()

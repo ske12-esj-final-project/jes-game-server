83c22f212d4363cc7377fae0977157108fd78bea
Raksit Mantanacharu <raksit.m@ku.th>
2018-04-25 18:43:13 +0700

    fixed bug killfeed

--- managers/player.js
+++ managers/player.js
@@ -218,7 +218,7 @@ module.exports = class {
 
         let victim = GameManager.getPlayer(targetId)
 
-        if (victim) {
+        if (victim && victim.hp > 0) {
             this.hitPlayer(victim, damage)
 
         } else {

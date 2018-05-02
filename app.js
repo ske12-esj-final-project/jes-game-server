c06b26d1517c98bf9defe901ff666d19199a5b74
b5710546232 <nattapat.su@ku.th>
2018-05-02 08:08:19 +0700

    change room name

--- app.js
+++ app.js
@@ -25,10 +25,10 @@ const gameWorldConfig1v1 = require('./config/gameworld1v1')
 const gameWorldConfig3v3 = require('./config/gameworld3v3')
 const gameWorldConfig10v10 = require('./config/gameworld10v10')
 
-let roomA = new Room(io, 'Room สอนหน่อยๆ', '0')
+let roomA = new Room(io, 'Room LookGoods', '0')
 let roomB = new Room(io, 'Room AR-TAG', '1',gameWorldConfig10v10)
-let room1v1 = new Room(io,'Room 1-1','2',gameWorldConfig1v1)
-let roomhod = new Room(io,'Room โหดจังครับ','3',gameWorldConfig1v1)
+let room1v1 = new Room(io,'Room Lenscape','2',gameWorldConfig1v1)
+let roomhod = new Room(io,'Room IVVA','3',gameWorldConfig1v1)
 let roomCookLab = new Room(io, 'Room คุกLab', '4',gameWorldConfig3v3)
 let roomManager
 

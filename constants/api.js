dfec5b2b5cf2804833372580560fa2405a4f74e6
b5710546232 <nattapat.su@ku.th>
2018-05-15 17:55:36 +0700

    update api

--- constants/api.js
+++ constants/api.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "USER": "http://jes.api.user.safesuk.me/v1/users",
-    "MATCH": "http://jes.api.match.safesuk.me/v1/matches",
-    "KILL": "http://jes.api.match.safesuk.me/v1/kills"
+    "USER": "http://jes.api.user.safesuk.fun/v1/users",
+    "MATCH": "http://jes.api.match.safesuk.fun/v1/matches",
+    "KILL": "http://jes.api.match.safesuk.fun/v1/kills"
 }
